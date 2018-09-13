3400351aafb6c9eed16a4f9e1843a2f9c36ec13b
Oratree <neem693@gmail.com>
2018-09-13 20:51:17 +0900

    update

--- 리눅스 참고자료/하둡/hadoop_exam/Couter_Exam/CountDriver.java
+++ 리눅스 참고자료/하둡/hadoop_exam/Couter_Exam/CountDriver.java
@@ -1,8 +1,7 @@
 package com.ic.mymapreducer;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,9 +9,11 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class CountDriver {
-	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+public class CountDriver extends Configured implements Tool{
+	/*public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 		Configuration conf = new Configuration();
 		if (args.length != 2) {
 			System.out.println("Usage : java CountDriver <in> <out>");
@@ -28,5 +29,32 @@ public class CountDriver {
 		job.setOutputValueClass(IntWritable.class);
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.waitForCompletion(true);
-	}
+	}*/
+	public static void main(String[] args) throws Exception {
+	     int res = ToolRunner.run(new Configuration(), new CountDriver(), args);
+	     System.exit(res);
+	  }
+
+	  public int run(String[] args) throws Exception
+	  {
+
+	    // Configuration processed by ToolRunner 
+	    Configuration conf = getConf();
+	    Job job = new Job(conf, "My Job");
+
+	    job.setJarByClass(CountDriver.class);
+		job.setMapperClass(CountMapper.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		job.setReducerClass(CountReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+	    return job.waitForCompletion(true) ? 0 : 1;
+	  }
+	
+	
+	
+	
 }
--- 리눅스 참고자료/하둡/hadoop_exam/Etc_Exam/ProcessUnits.java
+++ 리눅스 참고자료/하둡/hadoop_exam/Etc_Exam/ProcessUnits.java
@@ -20,13 +20,27 @@ public class ProcessUnits
    Text,                /*Output key Type*/ 
    IntWritable>        /*Output value Type*/ 
    { 
+
+      //splitting
+	  /*
+	  1  1979   23   23   2   43   24   25   26   26   26   26   25   26  25 
+	  2	 1980   26   27   28  28   28   30   31   31   31   30   30   30  29 
+	  3	 1981   31   32   32  32   33   34   35   36   36   34   34   34  34 
+	  4	 1984   39   38   39  39   39   41   42   43   40   39   38   38  40 
+	  5	 1985   38   39   39  39   39   41   41   41   00   40   39   39  45
+	  */
+
       
       //Map function 
       public void map(LongWritable key, Text value, 
       OutputCollector<Text, IntWritable> output,   
       Reporter reporter) throws IOException 
       { 
+         // key   value
+         //  1    1979   23   23   2   43   24   25   26   26   26   26   25   26  25 
+
          String line = value.toString(); 
+		 //     line = "1979   23   23   2   43   24   25   26   26   26   26   25   26  25"
          String lasttoken = null; 
          StringTokenizer s = new StringTokenizer(line," "); 
          String year = s.nextToken(); 
